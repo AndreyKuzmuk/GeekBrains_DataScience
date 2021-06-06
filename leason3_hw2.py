de781c623f2b0995bb162885c7499465bb55d29c
AndreyKuzmuk <gamingpro1701@gmail.com>
2021-06-07 02:24:58 +0300

    2

---
+++ leason3_hw2.py
@@ -0,0 +1,7 @@
+def bio(name, kwname, surname, kwsurname, born, kwborn, email, kwemail, number, kwnumber):
+    return name, kwname, surname, kwsurname, born, kwborn, email, kwemail, number, kwnumber
+
+
+print("Name:", input("Type your name: "), ", Surname:", input("Type your Surname: "), ", Born:",
+      input("Type date of your birth: "), ", Email:", input("Type your email: "), ", Number",
+      input("Type your number: "))
